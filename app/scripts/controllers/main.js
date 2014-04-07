e90683d83d564be7da824c5d34b842ef03dcec59
Ken Rimple <krimple@chariotsolutions.com>
2014-04-07 14:47:57 -0400

    basic functionality of mockup complete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,26 @@
 'use strict';
 
 angular.module('unkemptApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope) {
+
+  $scope.cards = [{
+    title: 'getting things done',
+    tasks: [{
+      complete: false,
+      description: "Laze around the house..."
+    },
+    {
+      complete: false,
+      description: "Go get some iced tea"
+    },
+    {
+      complete: true,
+      description: "Read the paper"
+    }
+    ]}
+  ];
+
+  $scope.deleteCard = function(card) {
+    $scope.cards.splice(card);
+  };
+});
